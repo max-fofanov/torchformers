923add501c79ad19dcbf54c1c25019da36874fb8
Max Fofanov <max.fofanov@gmail.com>
2023-09-16 15:42:10 +0300

    style: add info to setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,16 @@ setup(
     package_dir={"": "src"},
     packages=find_packages("src"),
     install_requires=["torch"],
+    author="Max Fofanov",
+    author_email="max.fofanov@gmail.com",
+    description="Minimalistic library for running transformer models in torch",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/max-fofanov/torchformers",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
