e76bc3348a8b2503b5d5b9892d396cfaa06fff81
Max Fofanov <max.fofanov@gmail.com>
2023-09-16 14:56:39 +0300

    fix: fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import torch
 from setuptools import setup, find_packages
+
 
 setup(
     name="transformer",
     version="0.1",
     packages=find_packages(),
-    install_requires=[torch],
+    install_requires=["torch"],
 )
