b3007234bdd2d6c4f56a39dbb1cff3794af375ff
Max Fofanov <max.fofanov@gmail.com>
2023-09-23 12:31:41 +0300

    chore: change version to 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchformers",
-    version="0.2.1",
+    version="0.2.2",
     package_dir={"": "src"},
     packages=find_packages("src"),
     install_requires=["torch"],
