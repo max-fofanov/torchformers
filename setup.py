61408a26fe01761c1a00a21d293cf190e1ec2563
Max Fofanov <max.fofanov@gmail.com>
2023-09-23 09:59:50 +0300

    chore: update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchformers",
-    version="0.1",
+    version="0.1.1",
     package_dir={"": "src"},
     packages=find_packages("src"),
     install_requires=["torch"],
@@ -18,5 +18,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
